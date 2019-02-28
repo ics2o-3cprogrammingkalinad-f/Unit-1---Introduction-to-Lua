29eab0db1a1128f300de8df8539619f9d0bfd501
Kalina Dunne Farrell <kalina.dunne-farrell@ocsbstudent.ca>
2019-02-28 08:27:43 -0500

    Added day 16+17

--- NumericTextFieldsKalinaD.F/main.lua
+++ NumericTextFieldsKalinaD.F/main.lua
@@ -1,8 +1,9 @@
 -----------------------------------------------------------------------------------------
--- Title: TouchAndReact
+-- Title: NumericTextFields
 -- Name: Kalina Dunne Farrell
 -- Course: ICS2O
--- This program
+-- This program displays a math question and asks the user to answer in a numeric 
+-- textfield terminal.
 -----------------------------------------------------------------------------------------
 
 -- Your code here
